9da9c66bf9ef379c4327e9515f1d80becd0717e1
ddcornwell <ddcornwell@gmail.com>
2016-07-26 02:46:45 -0500

    done

--- ruby/calculator_two.rb
+++ ruby/calculator_two.rb
@@ -1,8 +1,3 @@
-
-
-
-
-
 def cash(a,b,c)
  case b 
  when :+ then @@z  = a + c 
@@ -18,9 +13,11 @@ ok = []
 loop do 
 	
 puts "calculator"
-puts "needs a 'number opporator number' or 'done'"
+puts "needs a 'number opporator number'"
 puts "->"
+
 input = gets.chomp.split(" ")
+
 one = input[0].to_i 
 two = input[1].to_sym 
 three = input[2].to_i 
@@ -29,11 +26,15 @@ cash(one, two, three)
 ok << "#{one} #{two} #{three} = #{@@z} "
 puts 
 
-puts "keep going? type: y/n?"
+puts "keep going? type: y or done?"
 input = gets.chomp 
+until input == "y" || input == "done"
+puts "type: y or done?"
+input = gets.chomp 
+end 
 if input == "y"
 
-else input == "n" 
+else input == "done" 
 	puts "have a great day"
 	puts ok 
 	break
