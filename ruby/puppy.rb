479e9dccbff20c058c38641e07303c4b42951c4c
ddcornwell <ddcornwell@gmail.com>
2016-07-26 15:27:14 -0500

    working on classes

---
+++ ruby/puppy.rb
@@ -0,0 +1,53 @@
+# But how can we teach our puppy instances to fetch, roll over, or offer a high five? We can do this by writing our own instance methods -- which is exactly what we'll do in the next challenge. For now, submit the GitHub URL of puppy.rb to complete this challenge.
+
+class Puppy
+	def fetch 
+		"getting ball"
+	end
+	def rool_over
+		"spin"
+	end	
+	def hi_five
+		"here is my paw"
+	end
+end 
+
+sandy = Puppy.new 
+sandy.rool_over
+sandy.hi_five
+sandy.fetch
+
+
+
+
+
+# class Puppy
+# end
+
+# duchess = Puppy.new
+# fido = Puppy.new
+# spot = Puppy.new
+
+# spot.class
+
+# duchess == fido
+	
+# fido.instance_of?(Array)
+
+# fido.instance_of?(Puppy)
+
+# str = String.new
+# str.length
+# str.empty?
+# str += "huh."
+# str = "some string"
+
+# greeting = String.new("hello")
+
+# greeting.length
+
+# greeting.upcase
+
+
+
+
