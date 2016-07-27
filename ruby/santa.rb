5fe2a9f13ad11cc6db057657b8ff661bc89dd3f3
ddcornwell <ddcornwell@gmail.com>
2016-07-27 13:09:02 -0500

    ho ho ho

---
+++ ruby/santa.rb
@@ -0,0 +1,19 @@
+# Your class should have three instance methods:
+class Santa
+
+	def initialize 
+	end 
+# A speak method that will print "Ho, ho, ho! Haaaappy holidays!" (Santa has been working on being more inclusive.)
+	def speak 
+	puts "Ho, ho, ho! Haaaappy holidays!" 
+	end
+# An eat_milk_and_cookies method that takes a cookie type (example: "snickerdoodle") as a parameter and prints "That was a good <type of cookie here>!"
+def eat_milk_cookies( cookie_type )
+	puts "That was a good #{cookie_type}!"
+end 
+
+# An initialize method that prints "Initializing Santa instance ...".
+# Add a bit of code below your class declaration to check that you're able to initialize a Santa instance and call its methods.
+
+
+end 
