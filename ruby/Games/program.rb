d894260040c2367a5af6e28ca175ef8dad144b34
ddcornwell <ddcornwell@gmail.com>
2016-07-29 13:23:50 -0500

    clean up

--- ruby/Games/program.rb
+++ ruby/Games/program.rb
@@ -1,3 +1,9 @@
+# 1. The program requests a word or phrase pattern, e.g. "-------".
 
-class 
-5 + 3
+# 2. The program suggests a letter, or may guess the entire word or phrase.
+
+# 3. The user indicates which letter positions, if any, match that letter.
+#    If none match, a life is lost. If six (or configurable) lives are lost,
+#    the program loses.
+# Intro text, wait for user to type 'start'
+
--- ruby/shout.rb
+++ ruby/shout.rb
@@ -35,35 +35,70 @@
 # plane.take_off(30000)
 
 
-module Shout
-  def yell_angrily(word)
-  puts "#{word} What are you doing!!!.."
-  end
-end
+	
+			module Shout
+  				def yell_angrily(word)
+  				puts "#{word} What are you doing!!!.."
+  				end
+				end
 
-class Dan 
-	include Shout
+		class Dan 
+			include Shout
 
-def what 
-puts "it's not my fault"
-end
-end 
+			def what(name) 
+			puts "it's not #{name} fault type : yes / no"
+			can = gets.chomp
+				if can == "yes"
+					puts 
+					puts "it's all wasy your falt #{name}!"
+				else
+					puts 
+					puts "#{name}: am going to run a way.."
+					end 
+			end
+		end 
 
-class Mom 
-	include Shout
-def no 
-	puts "why are you helling for me!"
-end 
-	end 
+		class Mom 
+			include Shout
+			
+			def no(name) 
+			puts "why are you helling for #{name}! type: yes / no"
+			can = gets.chomp
+				if can == "yes"
+					puts 
+					puts "#{name} am Getting ready for Work!!"
+				else
+					puts 
+					puts "#{name} Make Dinner for the kids"
+					end 
+			end 
+		end 
+
+		class Dad
+			include Shout
+			def ok
+				puts "can we all get along? type: yes / no"
+				can = gets.chomp
+				if can == "yes"
+					puts 
+					puts "we just want our ipads back"
+				else
+					puts 
+					puts "Dad: 'No food for you...'"
+					end 
+			end 
+		end 
+
+		
+
  
 dan = Dan.new 
-dan.yell_angrily("Sandy")
-dan.what
+dan.yell_angrily("crap")
+dan.what("Daniel")
 puts 
 mom = Mom.new 
-mom.yell_angrily("Dan Paul Cornwell") 
-mom.no
-
+mom.yell_angrily("I am going kill you..") 
+mom.no("Sarah")
 	
 	
 	
