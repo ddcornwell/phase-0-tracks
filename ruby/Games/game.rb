4c718d9ff2e93919107aa63c8435e982d2c4cd00
ddcornwell <ddcornwell@gmail.com>
2016-07-29 22:24:27 -0500

    checking names

--- ruby/Games/game.rb
+++ ruby/Games/game.rb
@@ -3,10 +3,12 @@
 class Game
 	def player_one(name)
 		name = gets.chomp
+		name
 	end
 
 	def player_two(name)
 		 name = gets.chomp
+		 name
 	end
 
 	
--- ruby/Games/game_spec.rb
+++ ruby/Games/game_spec.rb
@@ -5,11 +5,11 @@ describe Game do
   let(:game) { Game.new }
 
   it "add one player_one" do
-    expect(game.player_one(name)).to eq "name"
+    expect(game.player_one(name)).to eq name
   end
 
   it "player_two" do
-    expect(game.player_two(name)).to eq "name"
+    expect(game.player_two(name)).to eq name
   end
 
 
