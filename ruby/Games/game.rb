27a9488923b53c8c5e97850d40e716887323ba0a
ddcornwell <ddcornwell@gmail.com>
2016-07-29 22:22:26 -0500

    working

--- ruby/Games/game.rb
+++ ruby/Games/game.rb
@@ -1,11 +1,4 @@
-# 1. The program requests a word or phrase pattern, e.g. "-------".
 
-# 2. The program suggests a letter, or may guess the entire word or phrase.
-
-# 3. The user indicates which letter positions, if any, match that letter.
-#    If none match, a life is lost. If six (or configurable) lives are lost,
-#    the program loses.
-# Intro text, wait for user to type 'start'
 
 class Game
 	def player_one(name)
@@ -16,20 +9,5 @@ class Game
 		 name = gets.chomp
 	end
 
-
-	# def subtract(x,y)
-	# 	x - y
-	# end
-
-
-
-	# def multiply(x,y)
-	# 	x * y
-	# end
-
-
-	# def divide(x,y)
-	# 	x / y
-	# end
 	
 end
--- ruby/Games/game_spec.rb
+++ ruby/Games/game_spec.rb
@@ -1,23 +1,17 @@
 require_relative 'game'
 
 
-describe game do
+describe Game do
   let(:game) { Game.new }
 
-  it "player_one" do
-    expect(game.player_one(name)).to eq name
+  it "add one player_one" do
+    expect(game.player_one(name)).to eq "name"
   end
 
   it "player_two" do
-    expect(game.player_two(name)).to eq name
+    expect(game.player_two(name)).to eq "name"
   end
 
-  # it "multiplies two integers" do
-  #   expect(calculator.multiply(2,3)).to eq 6
-  # end
 
-  # it "divide two integers" do
-  #   expect(calculator.divide(10,2)).to eq 5
-  # end
 end
 
