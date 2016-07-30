77416154ab1a849c5b357523f9106c87b5416922
ddcornwell <ddcornwell@gmail.com>
2016-07-30 16:38:55 -0500

    guess

--- ruby/game/game_spec.rb
+++ ruby/game/game_spec.rb
@@ -5,12 +5,12 @@ describe Game do
 
   it "pick number random " do
   	game = Game.new
-    expect(game.pick).to eq @hand
+    expect(game.pick="1").to eq "1"
   end
 
   it "hand looks at players pick" do
   	game = Game.new
-    expect(game.hand(@number)).to eq true
+    expect(game.hand("1")).to eq true
   end
 
 
