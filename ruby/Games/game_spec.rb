31465b4731fcf1e1ee17935589cc7ce0c07e29e0
ddcornwell <ddcornwell@gmail.com>
2016-07-30 00:35:57 -0500

    guess

--- ruby/Games/game_spec.rb
+++ ruby/Games/game_spec.rb
@@ -2,6 +2,10 @@ require_relative 'game'
 
 describe Game do
   let(:word, :letter) { Game.new }
+  def initialize
+    @word = word 
+    @letter = letter
+  end
 
   it "player_one" do
     expect(game.player_one("hello")).to eq "hello"
