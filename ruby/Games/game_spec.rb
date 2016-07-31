9f22e7a9d6c2d89fdf1c88e3076392b14795b477
ddcornwell <ddcornwell@gmail.com>
2016-07-30 21:25:08 -0500

    rspec change

--- ruby/Games/game_spec.rb
+++ ruby/Games/game_spec.rb
@@ -3,14 +3,14 @@ require_relative 'game'
 describe Game do
   let(:game) { Game.new }
 
-  it "pick number random " do
+  it  { should contain(1, 2, 3, 4, 5) } do
   	game = Game.new
-    expect(game.pick).to eq "1"
+    expect(game.pick).to eq 1
   end
 
   it "hand looks at players pick" do
   	game = Game.new
-    expect(game.hand(num)).to eq false 
+    expect(game.hand(1)).to eq true
   end
 
 
