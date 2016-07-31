bcb2833f4406598f65f16f69b2198849c2877a38
ddcornwell <ddcornwell@gmail.com>
2016-07-30 21:56:47 -0500

    rspec change

--- ruby/Games/game_spec.rb
+++ ruby/Games/game_spec.rb
@@ -5,7 +5,7 @@ describe Game do
 
   it "pick number random " do
   	game = Game.new
-    expect(game.pick).to eq "1"
+    expect(game.pick).to be <= 6
   end
 
   it "hand looks at players pick" do
