4c5ff13be403719944c2dbf425478107ecb658d5
ddcornwell <ddcornwell@gmail.com>
2016-07-30 21:36:24 -0500

    rspec change

--- ruby/Games/game_spec.rb
+++ ruby/Games/game_spec.rb
@@ -5,12 +5,12 @@ describe Game do
 
   it "pick number random " do
   	game = Game.new
-    expect(game.pick).to eq 
+    expect(game.pick).to eq be_kind_of(Fixnum)
   end
 
   it "hand looks at players pick" do
   	game = Game.new
-    expect(game.hand(num)).to be_kind_of(Fixnum)
+    expect(game.hand(Fixnum)).to be_kind_of(Fixnum)
   end
 
 
